61d8e2eeff2a40fc2cf3d42f0894a86a200aacb7
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-14 22:02:30 +0200

    Docs for deki-json from deki-editor f449cd3

--- navtreedata.js
+++ navtreedata.js
@@ -24,7 +24,12 @@
 */
 var NAVTREE =
 [
-  [ "deki-json", "index.html", ]
+  [ "deki-json", "index.html", [
+    [ "Changelog", "index.html#autotoc_md0", [
+      [ "0.15.0", "index.html#autotoc_md1", null ],
+      [ "Changed", "index.html#autotoc_md2", null ]
+    ] ]
+  ] ]
 ];
 
 var NAVTREEINDEX =
